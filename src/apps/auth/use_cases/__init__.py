39f7abc6aa70a01b52b3b54b3640bc61104d2f20
YodZ0 <timur.g00gle@gmail.com>
2025-01-21 14:19:22 +0500

    Create Login use case (stub)

---
+++ src/apps/auth/use_cases/__init__.py
@@ -0,0 +1,5 @@
+from .register import RegisterUseCaseProtocol as RegisterUseCaseProtocol
+from .register import RegisterUseCaseImpl as RegisterUseCaseImpl
+
+from .login import LoginUseCaseProtocol as LoginUseCaseProtocol
+from .login import LoginUseCaseImpl as LoginUseCaseImpl
