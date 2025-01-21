9a9cc19beabfa1aa950d4ec509cd8d46386f29e5
YodZ0 <timur.g00gle@gmail.com>
2025-01-21 14:17:05 +0500

    Create tokens schemas

---
+++ src/apps/auth/schemas/tokens.py
@@ -0,0 +1,11 @@
+from pydantic import BaseModel
+
+
+class AccessToken(BaseModel):
+    access_token: str
+    token_type: str = "Bearer"
+
+
+class RefreshToken(BaseModel):
+    refresh_token: str
+    token_type: str = "Bearer"
