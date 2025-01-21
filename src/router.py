9bdb97e371d88aa3b4afc1c0cab968ec8fbfe607
YodZ0 <timur.g00gle@gmail.com>
2025-01-21 14:20:31 +0500

    Include users router

--- src/router.py
+++ src/router.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
 from src.apps.healthcheck.router import router as healthcheck_router
+from src.apps.users.router import router as users_router
 
 
 def apply_routes(app: FastAPI) -> FastAPI:
     app.include_router(healthcheck_router)
+    app.include_router(users_router)
     return app
