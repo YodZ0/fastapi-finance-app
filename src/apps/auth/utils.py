e6959dc969fb0a4bbadc093aed84a953104703d1
YodZ0 <timur.g00gle@gmail.com>
2025-01-27 19:42:35 +0500

    Create JWT encode/decode funcs

---
+++ src/apps/auth/utils.py
@@ -0,0 +1,37 @@
+from datetime import timedelta, datetime, timezone
+
+import jwt
+
+from src.settings import settings
+
+
+def encode_jwt(
+    payload: dict,
+    private_key: str = settings.auth_jwt.private_key_path.read_text(),
+    algorithm: str = settings.auth_jwt.algorithm,
+    expire_minutes: int = settings.auth_jwt.access_token_expire_minutes,
+    expire_timedelta: timedelta | None = None,
+) -> str:
+    to_encode = payload.copy()
+    now = datetime.now(timezone.utc)
+
+    if expire_timedelta:
+        expire = now + expire_timedelta
+    else:
+        expire = now + timedelta(minutes=expire_minutes)
+
+    to_encode.update(
+        iat=now,
+        exp=expire,
+    )
+    encoded = jwt.encode(to_encode, private_key, algorithm=algorithm)
+    return encoded
+
+
+def decode_jwt(
+    token: str | bytes,
+    public_key: str = settings.auth_jwt.public_key_path.read_text(),
+    algorithm: str = settings.auth_jwt.algorithm,
+) -> dict:
+    decoded = jwt.decode(token, public_key, algorithms=[algorithm])
+    return decoded
